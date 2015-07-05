f209881be69bcf9b4d47c6f94a2117e1504d864c
Alex Dao <alex.dao@duke.edu>
2015-07-05 17:13:57 -0400

    Move pageradapter to main activity

--- app/src/main/java/com/lumivote/lumivote/ui/MainActivity.java
+++ app/src/main/java/com/lumivote/lumivote/ui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.lumivote.lumivote.ui;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -20,6 +22,7 @@ import com.lumivote.lumivote.R;
 import com.lumivote.lumivote.ui.about_tab.AboutFragment;
 import com.lumivote.lumivote.ui.bills_tab.BillsFragment;
 import com.lumivote.lumivote.ui.candidate_tab.CandidateListFragment;
+import com.lumivote.lumivote.ui.candidate_tab.CandidatePartyFragment;
 import com.lumivote.lumivote.ui.legislators_tab.LegislatorsFragment;
 import com.lumivote.lumivote.ui.timeline_tab.TimelineFragment;
 import com.lumivote.lumivote.ui.votes_tab.VotesFragment;
@@ -183,4 +186,36 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
     }
 
+    /**
+     * Created by alex on 6/30/15.
+     */
+    public static class TabsFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        final int PAGE_COUNT = 3;
+        private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+        private Context context;
+
+        public TabsFragmentPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+            tabTitles[0] = context.getString(R.string.candidates_democrat);
+            tabTitles[1] = context.getString(R.string.candidates_republican);
+            tabTitles[2] = context.getString(R.string.candidates_independent);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return CandidatePartyFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+    }
 }
