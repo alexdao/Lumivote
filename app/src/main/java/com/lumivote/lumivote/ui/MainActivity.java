a51fbda0b6e73c9241155b4db38a0a58cb5f5492
Alex Dao <alex.dao@duke.edu>
2015-08-09 22:58:15 -0700

    Add settings tab

--- app/src/main/java/com/lumivote/lumivote/ui/MainActivity.java
+++ app/src/main/java/com/lumivote/lumivote/ui/MainActivity.java
@@ -24,6 +24,7 @@ import com.lumivote.lumivote.ui.bills_tab.BillsFragment;
 import com.lumivote.lumivote.ui.candidate_tab.CandidateListFragment;
 import com.lumivote.lumivote.ui.candidate_tab.CandidatePartyFragment;
 import com.lumivote.lumivote.ui.legislators_tab.LegislatorsFragment;
+import com.lumivote.lumivote.ui.settings_tab.SettingsFragment;
 import com.lumivote.lumivote.ui.starred_tab.StarredFragment;
 import com.lumivote.lumivote.ui.timeline_tab.TimelineFragment;
 import com.lumivote.lumivote.ui.votes_tab.VotesFragment;
@@ -106,6 +107,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.votes:
                 fragmentClass = VotesFragment.class;
+                break;
+            case R.id.settings:
+                fragmentClass = SettingsFragment.class;
                 break;
             case R.id.about:
                 fragmentClass = AboutFragment.class;
---
+++ app/src/main/java/com/lumivote/lumivote/ui/settings_tab/SettingsFragment.java
@@ -0,0 +1,38 @@
+package com.lumivote.lumivote.ui.settings_tab;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lumivote.lumivote.R;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+        hideTabLayout();
+        return v;
+    }
+
+    private void hideTabLayout(){
+        TabLayout tabLayout = (TabLayout) getActivity().findViewById(R.id.tabLayout);
+        tabLayout.setVisibility(View.GONE);
+        ViewPager mViewPager = (ViewPager) getActivity().findViewById(R.id.viewpager);
+        mViewPager.setVisibility(View.GONE);
+    }
+}
