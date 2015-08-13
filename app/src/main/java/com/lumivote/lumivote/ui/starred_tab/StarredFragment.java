ac06d3ba5829135afd93e7bb9a18fef1a32e1b94
Alex Dao <alex.dao@duke.edu>
2015-08-13 15:43:58 -0700

    Implement recyclerview for starred fragment

--- app/src/main/java/com/lumivote/lumivote/ui/starred_tab/StarredFragment.java
+++ app/src/main/java/com/lumivote/lumivote/ui/starred_tab/StarredFragment.java
@@ -4,13 +4,33 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.lumivote.lumivote.R;
+import com.lumivote.lumivote.ui.DividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class StarredFragment extends Fragment {
+
+    private List<Data> starredData = new ArrayList<>();
+
+    @Bind(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    LinearLayoutManager llm;
+    RVAdapter adapter;
 
     public StarredFragment() {
         // Required empty public constructor
@@ -25,7 +45,11 @@ public class StarredFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_starred, container, false);
+        ButterKnife.bind(this, v);
         hideTabLayout();
+
+        setData();
+        initializeRecyclerView();
 
         return v;
     }
@@ -37,4 +61,107 @@ public class StarredFragment extends Fragment {
         mViewPager.setVisibility(View.GONE);
     }
 
+    private void setData(){
+        for(int i=0; i<10; i++){
+            starredData.add(new Data("main title", "main descrip", "leftTitle", "leftDescrip"));
+        }
+    }
+
+    private void initializeRecyclerView() {
+        adapter = new RVAdapter(starredData);
+        llm = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setAdapter(adapter);
+
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST);
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+
+    class Data {
+        String mainTitle;
+        String mainDescription;
+        String leftTitle;
+        String leftDescription;
+
+        Data(String mainTitle, String mainDescription, String leftTitle, String leftDescription) {
+            this.mainTitle = mainTitle;
+            this.mainDescription = mainDescription;
+            this.leftTitle = leftTitle;
+            this.leftDescription = leftDescription;
+        }
+    }
+
+    public static class RVAdapter extends RecyclerView.Adapter<RVAdapter.StarredDataViewHolder> {
+
+        List<Data> starredData;
+
+        RVAdapter(List<Data> starredData) {
+            this.starredData = starredData;
+        }
+
+        public void clear() {
+            starredData.clear();
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getItemCount() {
+            return starredData.size();
+        }
+
+        @Override
+        public StarredDataViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_starred, viewGroup, false);
+            StarredDataViewHolder pvh = new StarredDataViewHolder(v, new StarredDataViewHolder.ISunlightDataViewHolderClicks() {
+                public void onClickItem(View caller) {
+                    Log.d("Hello", "test");
+                }
+            });
+            return pvh;
+        }
+
+        @Override
+        public void onBindViewHolder(StarredDataViewHolder starredDataViewHolder, int i) {
+            starredDataViewHolder.mainTitle.setText(starredData.get(i).mainTitle);
+            starredDataViewHolder.mainDescription.setText(starredData.get(i).mainDescription);
+            starredDataViewHolder.leftTitle.setText(starredData.get(i).leftTitle);
+            starredDataViewHolder.leftDescription.setText(starredData.get(i).leftDescription);
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        public static class StarredDataViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            RelativeLayout relativeLayout;
+            TextView leftTitle;
+            TextView leftDescription;
+            TextView mainTitle;
+            TextView mainDescription;
+            public ISunlightDataViewHolderClicks mListener;
+
+            StarredDataViewHolder(View itemView, ISunlightDataViewHolderClicks listener) {
+                super(itemView);
+                mListener = listener;
+                relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relative_layout);
+                relativeLayout.setOnClickListener(this);
+                leftTitle = ButterKnife.findById(itemView, R.id.leftTitle);
+                leftDescription = ButterKnife.findById(itemView, R.id.leftDescription);
+                mainTitle = ButterKnife.findById(itemView, R.id.mainTitle);
+                mainDescription = ButterKnife.findById(itemView, R.id.mainDescription);
+            }
+
+            @Override
+            public void onClick(View v) {
+                mListener.onClickItem(v);
+            }
+
+            public interface ISunlightDataViewHolderClicks {
+                void onClickItem(View caller);
+            }
+        }
+    }
 }
