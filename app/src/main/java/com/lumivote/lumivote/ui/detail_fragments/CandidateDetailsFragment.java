2870e28948d65a88bc367535654a96a1d155f3f2
Alex Dao <alex.dao@duke.edu>
2015-08-20 16:02:26 -0700

    Add blank detail fragments for bills, candidates, and votes

---
+++ app/src/main/java/com/lumivote/lumivote/ui/detail_fragments/CandidateDetailsFragment.java
@@ -0,0 +1,24 @@
+package com.lumivote.lumivote.ui.detail_fragments;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lumivote.lumivote.R;
+
+public class CandidateDetailsFragment extends Fragment {
+
+    public CandidateDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_candidate_details, container, false);
+
+        return v;
+    }
+}
