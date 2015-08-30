33b8df80806b2ccab4478e9aed4b8b83d651df79
Alex Dao <alex.dao@duke.edu>
2015-08-30 12:36:58 -0400

    Skeleton rv added to timeline fragment Old rest client for lumivote deleted

--- app/src/main/java/com/lumivote/lumivote/ui/timeline_tab/TimelineFragment.java
+++ app/src/main/java/com/lumivote/lumivote/ui/timeline_tab/TimelineFragment.java
@@ -4,13 +4,33 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.lumivote.lumivote.R;
+import com.lumivote.lumivote.ui.DividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class TimelineFragment extends Fragment {
+
+    private List<Data> timelineData = new ArrayList<>();
+
+    @Bind(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    LinearLayoutManager llm;
+    RVAdapter adapter;
 
     public TimelineFragment() {
     }
@@ -18,15 +38,124 @@ public class TimelineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_timeline, container, false);
         hideTabLayout();
+        ButterKnife.bind(this, v);
 
-        return inflater.inflate(R.layout.fragment_timeline, container, false);
+        setData();
+        initializeRecyclerView();
+
+        return v;
     }
 
-    private void hideTabLayout(){
+    private void hideTabLayout() {
         TabLayout tabLayout = (TabLayout) getActivity().findViewById(R.id.tabLayout);
         tabLayout.setVisibility(View.GONE);
         ViewPager mViewPager = (ViewPager) getActivity().findViewById(R.id.viewpager);
         mViewPager.setVisibility(View.GONE);
+    }
+
+    private void setData() {
+        for (int i = 0; i < 10; i++) {
+            timelineData.add(new Data("hi", "test", "hello", "yes"));
+        }
+    }
+
+    private void initializeRecyclerView() {
+        adapter = new RVAdapter(timelineData);
+        llm = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setAdapter(adapter);
+
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST);
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+
+    class Data {
+        String mainTitle;
+        String mainDescription;
+        String leftTitle;
+        String leftDescription;
+
+        Data(String mainTitle, String mainDescription, String leftTitle, String leftDescription) {
+            this.mainTitle = mainTitle;
+            this.mainDescription = mainDescription;
+            this.leftTitle = leftTitle;
+            this.leftDescription = leftDescription;
+        }
+    }
+
+    public static class RVAdapter extends RecyclerView.Adapter<RVAdapter.TimelineDataViewHolder> {
+
+        List<Data> timelineData;
+
+        RVAdapter(List<Data> timelineData) {
+            this.timelineData = timelineData;
+        }
+
+        public void clear() {
+            timelineData.clear();
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getItemCount() {
+            return timelineData.size();
+        }
+
+        @Override
+        public TimelineDataViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_timeline, viewGroup, false);
+            TimelineDataViewHolder pvh = new TimelineDataViewHolder(v, new TimelineDataViewHolder.ISunlightDataViewHolderClicks() {
+                public void onClickItem(View caller) {
+                    Log.d("Hello", "test");
+                }
+            });
+            return pvh;
+        }
+
+        @Override
+        public void onBindViewHolder(TimelineDataViewHolder timelineDataViewHolder, int i) {
+            timelineDataViewHolder.mainTitle.setText(timelineData.get(i).mainTitle);
+            timelineDataViewHolder.mainDescription.setText(timelineData.get(i).mainDescription);
+            timelineDataViewHolder.leftTitle.setText(timelineData.get(i).leftTitle);
+            timelineDataViewHolder.leftDescription.setText(timelineData.get(i).leftDescription);
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        public static class TimelineDataViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            RelativeLayout relativeLayout;
+            TextView leftTitle;
+            TextView leftDescription;
+            TextView mainTitle;
+            TextView mainDescription;
+            public ISunlightDataViewHolderClicks mListener;
+
+            TimelineDataViewHolder(View itemView, ISunlightDataViewHolderClicks listener) {
+                super(itemView);
+                mListener = listener;
+                relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relative_layout);
+                relativeLayout.setOnClickListener(this);
+                leftTitle = ButterKnife.findById(itemView, R.id.leftTitle);
+                leftDescription = ButterKnife.findById(itemView, R.id.leftDescription);
+                mainTitle = ButterKnife.findById(itemView, R.id.mainTitle);
+                mainDescription = ButterKnife.findById(itemView, R.id.mainDescription);
+            }
+
+            @Override
+            public void onClick(View v) {
+                mListener.onClickItem(v);
+            }
+
+            public interface ISunlightDataViewHolderClicks {
+                void onClickItem(View caller);
+            }
+        }
     }
 }
